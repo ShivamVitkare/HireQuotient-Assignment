83974877c0b47d357daa22f64995efa423d2df71
Shivam-Vitkare <120474507+ShivamVitkare@users.noreply.github.com>
2023-12-01 19:38:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <List />
+      </header>
+    </div>
+  );
+}
+
+export default App;
